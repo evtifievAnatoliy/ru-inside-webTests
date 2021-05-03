6c9a7b047b48afb4acf31014fda9379c3a2af1a6
Anatoliy <Anatoliy@DESKTOP-H9LFQ4T>
2021-05-04 00:58:26 +0300

    addLog

--- src/main/java/ru/inside/webTests/App.java
+++ src/main/java/ru/inside/webTests/App.java
@@ -7,11 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import my.home.project.main.Main.RecordTread;
 
 public class App 
 {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	
 	public static void main(String[] args) throws InterruptedException {
 		
 		Thread thread = new RecordTread();
@@ -26,6 +29,8 @@ public class App
 			System.setProperty("webdriver.chrome.driver","D:\\Java\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
 			try {
+				
+				logger.info("Test: ");
 				driver.get("https://gorzdrav.spb.ru/service-free-schedule#%5B%7B%22district%22%3A4%7D%5D");
 				
 				List<WebElement> districtInputElementList = driver.findElements(By.className("service-districts-list__item"));
@@ -42,7 +47,7 @@ public class App
 			
 //				inputElement.submit();
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				logger.info("False: " + e.getMessage());
 			}
 			finally {
 				//driver.quit();
